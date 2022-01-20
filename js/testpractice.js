727f5e78b0218f9001c9f463beaad477fb6963b4
Vityamin-N <heartless41196@gmail.com>
2022-01-20 21:27:47 +0200

    Четные числа

---
+++ js/testpractice.js
@@ -0,0 +1,13 @@
+// //Возвращение массива всех четных чисел от start до end
+// function getEvenNumbers(start, end) {
+//    const arr = [];
+//    for(let i = start; i <= end; i += 1){
+//      if(i % 2 === 0){
+//        arr.push(i);
+//      }
+//    }
+//    return arr;
+//    }
+//    console.log(getEvenNumbers(2, 5));
+//    console.log(getEvenNumbers(3, 11));
+//    console.log(getEvenNumbers(6, 12));
