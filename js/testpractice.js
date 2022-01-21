407dcf2643d3211bc84dbee552a1baecb1d58c8c
Vityamin-N <heartless41196@gmail.com>
2022-01-21 13:06:23 +0200

    Получение массива значений объекта

--- js/testpractice.js
+++ js/testpractice.js
@@ -30,3 +30,23 @@
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 // console.log(findLongestWord("Google do a roll"));
 // console.log(findLongestWord("May the force be with you"));
+
+// // propName - имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getAllPropValues(propName) {
+//     // Change code below this line
+//    const arr = [];
+//    for(const product of products){
+//      if(product.hasOwnProperty(propName)){
+//       arr.push(product[propName]);
+//       }
+//    }
+//     return arr;
+//     // Change code above this line
+//   }
