a651f2285ee42d88c1fece02713cf6cedcdd7e80
Vityamin-N <heartless41196@gmail.com>
2022-01-21 02:58:08 +0200

    Самое длинное слово в строке

--- js/testpractice.js
+++ js/testpractice.js
@@ -11,3 +11,22 @@
 //    console.log(getEvenNumbers(2, 5));
 //    console.log(getEvenNumbers(3, 11));
 //    console.log(getEvenNumbers(6, 12));
+
+// // Вывод самого длинного слова в строке
+// function findLongestWord(string) {
+//     // Change code below this line
+//     const word = string.split(" ");
+//     let longestWord = word[0];
+//     for(let i = 0; i < word.length; i += 1){
+//       if(word[i].length > longestWord.length){
+//         longestWord = word[i];
+//       }
+//     }
+//     return longestWord;
+  
+//     // Change code above this line
+//   }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
