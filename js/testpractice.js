a0463286aae6bce71553c2f05f4ef83961383528
Vityamin-N <heartless41196@gmail.com>
2022-01-23 04:52:53 +0200

    Вызов функций в массиве объектов

--- js/testpractice.js
+++ js/testpractice.js
@@ -12,6 +12,7 @@
 //    console.log(getEvenNumbers(3, 11));
 //    console.log(getEvenNumbers(6, 12));
 
+
 // // Вывод самого длинного слова в строке
 // function findLongestWord(string) {
 //     // Change code below this line
@@ -30,6 +31,7 @@
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 // console.log(findLongestWord("Google do a roll"));
 // console.log(findLongestWord("May the force be with you"));
+
 
 // // propName - имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products
 // const products = [
@@ -50,3 +52,48 @@
 //     return arr;
 //     // Change code above this line
 //   }
+
+// //Вызов функций в массиве объектов
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//       for(let elem of this.potions){
+//         if (elem.name === newPotion.name) {
+//             return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//           }
+//       }
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(potionName) {
+//     for (let i = 0; i < this.potions.length; i += 1) {
+//         if (potionName === this.potions[i].name) {
+//           this.potions.splice(i, 1);
+//           return `Potion ${potionName} is not in inventory!`;
+//         }
+//     }  
+//   },
+
+//   updatePotionName(oldName, newName) {
+//     //const potionIndex = this.potions.indexOf(oldName);
+//     for (let potion of this.potions) {
+//         if (oldName === potion.name) {
+//             //this.potions.splice(i, 1, newName);
+//             potion.name = newName;
+//             return `Potion ${oldName} is not in inventory!`;
+//           }
+//     }
+//   },
+// //   Change code above this line
+// };
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+// console.log(atTheOldToad.potions);
+// atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion");
+// console.log(atTheOldToad.potions);
